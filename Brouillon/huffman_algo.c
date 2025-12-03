b8494d7915096046916342e0b88124f6d5502c36
FlorentCHEN <florent.chen94@gmail.com>
2025-12-03 18:37:30 +0100

    Refactor Huffman tree implementation
    Refactor Huffman tree structure and functions for better clarity and functionality.

--- Brouillon/huffman_algo.c
+++ Brouillon/huffman_algo.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SPECIALES -2
+#define NOEUD_INT -1
 //utf8
 typedef struct Noeud {
     int frequence;//char
@@ -9,13 +11,14 @@ typedef struct Noeud {
     struct Noeud *parent;
     struct Noeud *gauche;//next
     struct Noeud *droite;//next
-    int gdbh;
 } Noeud;
 
 typedef struct {
     Noeud *racine;
     Noeud *special; //acces a la # , O(1)
-    Noeud *tab_noeud[256];// un tas , mais je n'ai pas encore regarder comment faire le lien avec l'arbre et le tab
+    Noeud *tab_feuille[256];// un tas , mais je n'ai pas encore regarder comment faire le lien avec l'arbre et le tab
+    Noeud *liste_noeud[512];//l ensemble des noeuds
+    int nb_noeuds;// combien de noeud il y adans liste_noeud
 } ArbreH;
 
 Noeud* buildNoeud(int symbole, int freq,Noeud *parent ,int cpt){
@@ -27,84 +30,89 @@ Noeud* buildNoeud(int symbole, int freq,Noeud *parent ,int cpt){
     noeuds->parent = parent;
     noeuds->gauche = NULL;
     noeuds->droite = NULL;
-    noeuds->gdbh = cpt;
+
     return noeuds;
 }
 
-ArbreH* buildArbreH(){
-    //initialise un arbre de huffman
-    ArbreH* arbre = malloc(sizeof(ArbreH));
-    if(!arbre)return NULL;
-    arbre->racine = buildNoeud(-1,0,NULL,0);// # = -1
-    arbre->special = arbre->racine;
-    for (int i = 0; i<256; i++)arbre->tab_noeud[i]=NULL;
+ArbreH* buildArbreH() {
+    ArbreH* arbre = calloc(1,sizeof(ArbreH));
+    if (!arbre) return NULL;
+    Noeud* n = buildNoeud(SPECIALES,0,NULL,0);
+    if (!n){
+        free(arbre);
+        return NULL;
+    }
+    arbre->racine = n;
+    arbre->special = n;
+    arbre->liste_noeud[0]=n;
+    arbre->nb_noeuds = 1;
     return arbre;
 }
 
-void recherche_finbloc(Noeud* noeuds, Noeud** noeuds_target, int freq_target){
-    //recherche du meilleur target dans l'arbre si il y a une égalité il retourne le gdbh le plus haut
-    if (noeuds == NULL)return;
-    if(noeuds->frequence == freq_target && (*noeuds_target == NULL || noeuds->gdbh>(*noeuds_target)->gdbh)){
-        *noeuds_target=noeuds;
+int trouver_index(ArbreH* h, Noeud* n) {
+    //retourner l'index du noeud
+    for (int i = 0; i < h->nb_noeuds; i++) {
+        if (h->liste_noeud[i] == n) return i;
     }
-    recherche_finbloc(noeuds->droite,noeuds_target,freq_target);
-    recherche_finbloc(noeuds->gauche,noeuds_target,freq_target);
-    return;
+    return -1;
 }
 
-Noeud* finBloc(ArbreH* arbre, int freq){
-    //Permet de lancer la fonction recherche_finbloc
-    Noeud* target = NULL;
-    recherche_finbloc(arbre->racine,&target,freq);
-    return target;
-}
+Noeud* finBloc(ArbreH* h, Noeud* m) {
+    //retourner le noeud qui a la meme frequence et un classe superieur a lui
+    int i = trouver_index(h, m);
+    int poids = m->frequence;
+    Noeud* noeuds_target= NULL;
 
-void update_GDBH(Noeud* n, int* cpt) {
-    // il met a jour le gdbh de chaque noeud
-    if (n == NULL) return;
-    
-    update_GDBH(n->gauche, cpt);
-    update_GDBH(n->droite, cpt);
-    
-    n->gdbh = *cpt;
-    (*cpt)++;
-}
-
-void numerotation_GDBH(ArbreH* arbre) {
-    //lance la mise a jour du gdbh
-    int cpt = 1;
-    update_GDBH(arbre->racine,&cpt);
+    while (i < h->nb_noeuds) {
+        Noeud* candidat = h->liste_noeud[i];
+        if(candidat->frequence == poids){
+            noeuds_target=candidat;
+        }
+        i++;
+    }
+    return noeuds_target;
 }
 
 void echange_noeud(ArbreH* h, Noeud* n1, Noeud* n2){
-    //echange de place entre 2 noeuds
+    //on echange de place 2 noeuds
     if (h == NULL || n1 == NULL || n2 == NULL ||n1==n2) return; 
     
     Noeud* parent1 = n1->parent;
     Noeud* parent2 = n2->parent;
-    
+
     int n1_gauche = (parent1 && parent1->gauche == n1);
     int n2_gauche = (parent2 && parent2->gauche == n2);
+    int idx_n1 = trouver_index(h, n1);
+    int idx_n2 = trouver_index(h, n2);
+
+    h->liste_noeud[idx_n1] = n2;
+    h->liste_noeud[idx_n2] = n1;
+
+    if (parent1) {
+        if (n1_gauche) parent1->gauche = n2;
+        else parent1->droite = n2;
+    } else h->racine = n2;
     
-    if (n1_gauche)parent1->gauche = n2;
-    else parent1->droite  = n2;
-    
-    if (n2_gauche)parent2->gauche = n1;
-    else parent2->droite  = n1;
+    if (parent2) {
+        if (n2_gauche) parent2->gauche = n1;
+        else parent2->droite = n1;
+    } else h->racine = n1;
     
     n1->parent = parent2;
     n2->parent = parent1;
 }
 
 void traitement(ArbreH* h, Noeud* q) {
-    //
+    //verifie si il respect les propriete de huffman sinon il le corrige
     if (q == NULL ||h == NULL) return;
     if (q == h->racine){
         q->frequence++;
         return;
     }
-    Noeud* b = finBloc(h, q->frequence);
-    if (b != q && q->parent != b)echange_noeud(h, q, b);
+    Noeud* b = finBloc(h, q);
+    if (b != q && q->parent != b){
+        echange_noeud(h, q, b);
+    }
     q->frequence++; 
     traitement(h, q->parent);
 }
